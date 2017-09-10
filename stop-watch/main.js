86e2e95207e993ae4e4ee52d6c0a22886cac5f0e
Alexander Lee <alex8usa8@gmail.com>
2017-09-10 14:42:43 -0700

    Massive Rework of JavaScript File. Added More Features to Stop Watch #4

--- stop-watch/main.js
+++ stop-watch/main.js
@@ -2,80 +2,94 @@ var millisecondsIntervalId
 var secondsIntervalId
 var minutesInterValId
 
-var $minutes = document.querySelector('#minutes')
-var $seconds = document.querySelector('#seconds')
-var $milliseconds = document.querySelector('#milliseconds')
+var $minutesHtml = document.querySelector('#minutes')
+var $minutes = parseInt($minutesHtml.textContent, 10)
+var $secondsHtml = document.querySelector('#seconds')
+var $seconds = parseInt($secondsHtml.textContent, 10)
+var $millisecondsHtml = document.querySelector('#milliseconds')
+var $milliseconds = parseInt($millisecondsHtml.textContent, 10)
 
 var $startButton = document.querySelector('#startButton')
 var $stopButton = document.querySelector('#stopButton')
 var $resetButton = document.querySelector('#resetButton')
 
 var $timeLimit = document.querySelector('#timeLimit')
-var $minutesLimit = '0'
-var $secondsLimit = '0'
-var $millisecondsLimit = '0'
+var $displayTimeLimit = document.querySelector('#displayTimeLimit')
+var $submissionError = document.querySelector('#submissionError')
+var $minutesLimit
+var $secondsLimit
+var $millisecondsLimit
 
 function milliseconds () {
-  var number = parseInt($milliseconds.textContent, 10)
-  if (($minutes.textContent === $minutesLimit) &&
-    ($seconds.textContent === $secondsLimit) &&
-    ($milliseconds.textContent === $millisecondsLimit)) {
+  if (($minutes === $minutesLimit) &&
+    ($seconds === $secondsLimit) &&
+    ($milliseconds === $millisecondsLimit)) {
     clearInterval(millisecondsIntervalId)
     clearInterval(secondsIntervalId)
     clearInterval(minutesInterValId)
   }
   else {
-    if (number < 9) {
-      $milliseconds.textContent = number + 1
+    if ($milliseconds < 9) {
+      $milliseconds = $milliseconds + 1
+      $millisecondsHtml.textContent = $milliseconds
     }
     else {
-      $milliseconds.textContent = 0
+      $milliseconds = 0
+      $millisecondsHtml.textContent = 0
     }
   }
 }
 
 function seconds () {
-  var number = parseInt($seconds.textContent, 10)
-  if (number < 59) {
-    if (number < 9) {
-      $seconds.textContent = '0' + (number + 1)
+  if ($seconds < 59) {
+    if ($seconds < 9) {
+      $seconds = $seconds + 1
+      $secondsHtml.textContent = '0' + $seconds
     }
     else {
-      $seconds.textContent = number + 1
+      $seconds = $seconds + 1
+      $secondsHtml.textContent = $seconds
     }
   }
   else {
-    $seconds.textContent = '00'
+    $seconds = 0
+    $secondsHtml.textContent = '00'
   }
 }
 
 function minutes () {
-  var number = parseInt($minutes.textContent, 10)
-  if (number < 59) {
-    if (number < 9) {
-      $minutes.textContent = '0' + (number + 1)
+  if ($minutes < 59) {
+    if ($minutes < 9) {
+      $minutes = $minutes + 1
+      $minutesHtml.textContent = '0' + $minutes
     }
     else {
-      $minutes.textContent = number + 1
+      $minutes = $minutes + 1
+      $minutesHtml.textContent = $minutes
     }
   }
   else {
-    $minutes.textContent = '00'
+    $minutes = 0
+    $minutesHtml.textContent = '00'
   }
 }
 
 function stopWatchLimit (event){
   event.preventDefault()
-  $minutesLimit = document.querySelector('#minutesLimit').value
-  $secondsLimit = document.querySelector('#secondsLimit').value
-  $millisecondsLimit = document.querySelector('#millisecondsLimit').value
+  var checkMinutes = parseInt(document.querySelector('#minutesLimit').value, 10)
+  var checkSeconds = parseInt(document.querySelector('#secondsLimit').value, 10)
+  var checkMilliseconds = parseInt(document.querySelector('#millisecondsLimit').value, 10)
 
-  if (parseInt($minutesLimit, 10) < 10) {
-    $minutesLimit = '0' + $minutesLimit
+  if ((checkMinutes < 60) && (checkSeconds < 60) & (checkMilliseconds < 10)) {
+    $submissionError.className = 'hideError'
+    $minutesLimit = checkMinutes
+    $secondsLimit = checkSeconds
+    $millisecondsLimit = checkMilliseconds
+    $displayTimeLimit.textContent = $minutesLimit + ' Minutes, ' + $secondsLimit
+      + ' Seconds, ' + $millisecondsLimit + ' Milliseconds'
   }
-
-  if (parseInt($secondsLimit, 10) < 10) {
-    $secondsLimit = '0' + $secondsLimit
+  else {
+    $submissionError.className = 'showError'
   }
 }
 $startButton.addEventListener('click', function () {
@@ -91,9 +105,12 @@ $stopButton.addEventListener('click', function () {
 })
 
 $resetButton.addEventListener('click', function () {
-  $milliseconds.textContent = '0'
-  $seconds.textContent = '00'
-  $minutes.textContent = '00'
+  $millisecondsHtml.textContent = '0'
+  $secondsHtml.textContent = '00'
+  $minutesHtml.textContent = '00'
+  $milliseconds = 0
+  $seconds = 0
+  $minutes = 0
 })
 
 $timeLimit.addEventListener('submit', stopWatchLimit)
